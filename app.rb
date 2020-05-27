c4f528be906cf7f0394e6b86f70247651a440791
MarcRodrigFelix <m.rodrig.felix@gmail.com>
2020-05-27 00:26:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -13,7 +13,6 @@ class App < Sinatra::Base
     @words = @analyzed_text.count_of_words
     @vowels = @analyzed_text.count_of_vowels
     @const = @analyzed_text.count_of_consonants
-    
     @letter = @analyzed_text.most_used_letter.keys.join("")
     @letter_count = @analyzed_text.most_used_letter.values.join("")
 
